2ef87256feaf026b52e1fadad24688a9b33fdcf2
Sachin waghmare <sachin.waghmare0227@gmail.com>
2024-07-03 18:34:54 +0530

    user2 commit

---
+++ src/test/java/FW24/Tests/GitWriteaaaAndUplodCodeUser2_1.java
@@ -0,0 +1,17 @@
+package FW24.Tests;
+
+import org.testng.annotations.Test;
+
+import FW24.baseTest.BaseTest;
+
+public class GitWriteaaaAndUplodCodeUser2_1 extends BaseTest {
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("Test 1 line 1 ");
+		System.out.println("Test 1 line 2");
+		System.out.println("Test 1 line 3");
+	}
+
+}
